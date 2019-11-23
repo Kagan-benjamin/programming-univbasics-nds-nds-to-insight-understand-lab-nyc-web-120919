fb9ef3e25123e511ca2bce417fa96a2b8c1af62f
Benjamin Kagan <kagan.benjamin@gmail.com>
2019-11-23 18:54:57 +0000

    Automatically backed up by Learn

--- lib/directors_database.rb
+++ lib/directors_database.rb
@@ -4,3 +4,11 @@ def directors_database
 	rot13 = -> (s) { s.tr('A-Za-z', 'N-ZA-Mn-za-m') }
   @_db ||= YAML.load(rot13.call(File.open("directors_db").read.to_s))
 end
+
+require 'pp'
+
+def pretty_print_nds(nds) 
+ pp nds 
+   
+end
+
